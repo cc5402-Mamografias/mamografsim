d567acd9982f0ef7d97e77a71473deab80da4ed7
Felipe Canales <felipe.canales.213@gmail.com>
2020-09-28 12:02:48 -0300

    first commit

---
+++ lib.php
@@ -0,0 +1,204 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Library of interface functions and constants.
+ *
+ * @package     mod_mamografsim
+ * @copyright   2020 cc5402-Mamografías
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Return if the plugin supports $feature.
+ *
+ * @param string $feature Constant representing the feature.
+ * @return true | null True if the feature is supported, null otherwise.
+ */
+function mamografsim_supports($feature) {
+    switch ($feature) {
+        case FEATURE_GRADE_HAS_GRADE:
+            return true;
+        case FEATURE_MOD_INTRO:
+            return true;
+        default:
+            return null;
+    }
+}
+
+/**
+ * Saves a new instance of the mod_mamografsim into the database.
+ *
+ * Given an object containing all the necessary data, (defined by the form
+ * in mod_form.php) this function will create a new instance and return the id
+ * number of the instance.
+ *
+ * @param object $moduleinstance An object from the form.
+ * @param mod_mamografsim_mod_form $mform The form.
+ * @return int The id of the newly inserted record.
+ */
+function mamografsim_add_instance($moduleinstance, $mform = null) {
+    global $DB;
+
+    $moduleinstance->timecreated = time();
+
+    $id = $DB->insert_record('mamografsim', $moduleinstance);
+
+    return $id;
+}
+
+/**
+ * Updates an instance of the mod_mamografsim in the database.
+ *
+ * Given an object containing all the necessary data (defined in mod_form.php),
+ * this function will update an existing instance with new data.
+ *
+ * @param object $moduleinstance An object from the form in mod_form.php.
+ * @param mod_mamografsim_mod_form $mform The form.
+ * @return bool True if successful, false otherwise.
+ */
+function mamografsim_update_instance($moduleinstance, $mform = null) {
+    global $DB;
+
+    $moduleinstance->timemodified = time();
+    $moduleinstance->id = $moduleinstance->instance;
+
+    return $DB->update_record('mamografsim', $moduleinstance);
+}
+
+/**
+ * Removes an instance of the mod_mamografsim from the database.
+ *
+ * @param int $id Id of the module instance.
+ * @return bool True if successful, false on failure.
+ */
+function mamografsim_delete_instance($id) {
+    global $DB;
+
+    $exists = $DB->get_record('mamografsim', array('id' => $id));
+    if (!$exists) {
+        return false;
+    }
+
+    $DB->delete_records('mamografsim', array('id' => $id));
+
+    return true;
+}
+
+/**
+ * Is a given scale used by the instance of mod_mamografsim?
+ *
+ * This function returns if a scale is being used by one mod_mamografsim
+ * if it has support for grading and scales.
+ *
+ * @param int $moduleinstanceid ID of an instance of this module.
+ * @param int $scaleid ID of the scale.
+ * @return bool True if the scale is used by the given mod_mamografsim instance.
+ */
+function mamografsim_scale_used($moduleinstanceid, $scaleid) {
+    global $DB;
+
+    if ($scaleid && $DB->record_exists('mamografsim', array('id' => $moduleinstanceid, 'grade' => -$scaleid))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Checks if scale is being used by any instance of mod_mamografsim.
+ *
+ * This is used to find out if scale used anywhere.
+ *
+ * @param int $scaleid ID of the scale.
+ * @return bool True if the scale is used by any mod_mamografsim instance.
+ */
+function mamografsim_scale_used_anywhere($scaleid) {
+    global $DB;
+
+    if ($scaleid and $DB->record_exists('mamografsim', array('grade' => -$scaleid))) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Creates or updates grade item for the given mod_mamografsim instance.
+ *
+ * Needed by {@see grade_update_mod_grades()}.
+ *
+ * @param stdClass $moduleinstance Instance object with extra cmidnumber and modname property.
+ * @param bool $reset Reset grades in the gradebook.
+ * @return void.
+ */
+function mamografsim_grade_item_update($moduleinstance, $reset=false) {
+    global $CFG;
+    require_once($CFG->libdir.'/gradelib.php');
+
+    $item = array();
+    $item['itemname'] = clean_param($moduleinstance->name, PARAM_NOTAGS);
+    $item['gradetype'] = GRADE_TYPE_VALUE;
+
+    if ($moduleinstance->grade > 0) {
+        $item['gradetype'] = GRADE_TYPE_VALUE;
+        $item['grademax']  = $moduleinstance->grade;
+        $item['grademin']  = 0;
+    } else if ($moduleinstance->grade < 0) {
+        $item['gradetype'] = GRADE_TYPE_SCALE;
+        $item['scaleid']   = -$moduleinstance->grade;
+    } else {
+        $item['gradetype'] = GRADE_TYPE_NONE;
+    }
+    if ($reset) {
+        $item['reset'] = true;
+    }
+
+    grade_update('/mod/mamografsim', $moduleinstance->course, 'mod', 'mod_mamografsim', $moduleinstance->id, 0, null, $item);
+}
+
+/**
+ * Delete grade item for given mod_mamografsim instance.
+ *
+ * @param stdClass $moduleinstance Instance object.
+ * @return grade_item.
+ */
+function mamografsim_grade_item_delete($moduleinstance) {
+    global $CFG;
+    require_once($CFG->libdir.'/gradelib.php');
+
+    return grade_update('/mod/mamografsim', $moduleinstance->course, 'mod', 'mamografsim',
+                        $moduleinstance->id, 0, null, array('deleted' => 1));
+}
+
+/**
+ * Update mod_mamografsim grades in the gradebook.
+ *
+ * Needed by {@see grade_update_mod_grades()}.
+ *
+ * @param stdClass $moduleinstance Instance object with extra cmidnumber and modname property.
+ * @param int $userid Update grade of specific user only, 0 means all participants.
+ */
+function mamografsim_update_grades($moduleinstance, $userid = 0) {
+    global $CFG, $DB;
+    require_once($CFG->libdir.'/gradelib.php');
+
+    // Populate array of grade objects indexed by userid.
+    $grades = array();
+    grade_update('/mod/mamografsim', $moduleinstance->course, 'mod', 'mod_mamografsim', $moduleinstance->id, 0, $grades);
+}
