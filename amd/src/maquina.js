ecddd688f6438134f6900c5d874fef99d93587d8
Felipe Canales <felipe.canales.213@gmail.com>
2020-10-05 11:45:19 -0300

    Nuevos parámetros

--- amd/src/maquina.js
+++ amd/src/maquina.js
@@ -1,43 +1,65 @@
 define(['mod_mamografsim/herramientas', 'mod_mamografsim/vista'], function (hs, v) {
   // TODO rango superior altura
   //      rango inferior altura
-  //      altura predeterminada
+  //      valores defecto (reemplazar nulls)
   //      construir estado
   //      no activar si esta muy alto?
-  //      inicializar errores
+  //      no deseleccionar herramienta si la altura es muy baja
 
   var herramienta = new hs.HerramNull();
+
+  
   var alturaCompresor = null;
+  var fuerza = null;
+  var kilovolt = null;
+  var miliamperios = null;
+  var errorKilovolt = 0;
+  var errorMiliamperios = 0;
 
   function construirEstado(activo) {
 
   }
 
-  function actualizarHerramienta(activo = false) {
-    herramienta.actualizar(construirEstado(activo));
+  function actualizar(activo = false) {
+    e = construirEstado(activo)
+    herramienta.actualizar(e);
+    v.dibujarMaquina(e);
   }
 
   return {
+    // Inicializa los errores
+    init: function(errorkv, errorma) {
+      errorKilovolt = errorkv;
+      errorMiliamperios = errorma;
+    },
+    // Setea los parametros del panel de control
+    setearParams: function(kv, ma) {
+      kilovolt = kv;
+      miliamperios = ma;
+      actualizar();
+    },
+    // Selecciona una nueva herramienta o deselecciona la antigua
     setHerramienta: function(herram) {
       herramienta = herram;
-      actualizarHerramienta()
+      actualizar()
     },
+    // Entrega la herramienta actual
     getHerramienta: function() {
       return herramienta;
     },
+    // Sube el compresor
     subirCompresor: function() {
       alturaCompresor -= 1;
-      actualizarHerramienta()
+      actualizar()
     },
+    // Baja el compresor
     bajarCompresor: function() {
       alturaCompresor += 1;
-      actualizarHerramienta()
+      actualizar()
     },
+    // Activa el mamografo
     activar: function() {
-      actualizarHerramienta(activo=true);
-    },
-    dibujar: function() {
-      v.dibujarMaquina(construirEstado(false));
+      actualizar(activo=true);
     }
   }
 });
