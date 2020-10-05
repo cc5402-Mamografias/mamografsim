d2015bdef9b5fef89789df1f032bdf5856b12ac1
javiera <javiera.diaz.1@ug.uchile.cl>
2020-10-05 12:30:30 -0300

    seccion de modos agregada

---
+++ amd/src/control-panel.js
@@ -0,0 +1,32 @@
+function sub(element){
+    var to_sub = document.getElementById(element.value).value;
+    if (to_sub > 1){
+        var new_value = parseFloat(to_sub) - 1;
+        document.getElementById(element.value).value = new_value.toString();
+    }
+}
+
+function add(element){
+    var to_add = document.getElementById(element.value).value;
+    if (to_add < 5){
+        var new_value = parseFloat(to_add) + 1;
+        document.getElementById(element.value).value = new_value.toString();
+    }
+}
+
+function changeMode(){
+    let modes = ["c01", "c02", "c03", "c04"];
+    
+    for(var i = 0; i<4; i++){
+        if(document.getElementById(modes[i]).checked==true){
+            var index=i; 
+        }
+    }
+    if(typeof index == 'undefined'){
+        document.getElementById(modes[0]).checked=true;
+    }
+    else{
+        document.getElementById(modes[index]).checked=false;
+        document.getElementById(modes[(index+1)%4]).checked=true;
+    }
+}
