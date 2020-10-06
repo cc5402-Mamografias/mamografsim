51bbf5bc62094811362e3ca4e61cffe094eec895
GCarboni96 <gianluca.carboni@ing.uchile.cl>
2020-10-06 12:33:18 -0300

    error de botones debuggeado. Parametros de filtro y anodo agregados.

--- amd/src/control-panel.js
+++ amd/src/control-panel.js
@@ -8,7 +8,7 @@ function sub(element){
 
 function add(element){
     var to_add = document.getElementById(element.value).value;
-    if (to_add < 5){
+    if (to_add < 200){
         var new_value = parseFloat(to_add) + 1;
         document.getElementById(element.value).value = new_value.toString();
     }
@@ -28,5 +28,39 @@ function changeMode(){
     else{
         document.getElementById(modes[index]).checked=false;
         document.getElementById(modes[(index+1)%4]).checked=true;
+    }
+}
+
+function changeFilter(){
+    let modes = ["f01", "f02", "f03"];
+
+    for(var i = 0; i<3; i++){
+        if(document.getElementById(modes[i]).checked==true){
+            var index=i;
+        }
+    }
+    if(typeof index == 'undefined'){
+        document.getElementById(modes[0]).checked=true;
+    }
+    else{
+        document.getElementById(modes[index]).checked=false;
+        document.getElementById(modes[(index+1)%3]).checked=true;
+    }
+}
+
+function changeAnode(){
+    let modes = ["a01", "a02", "a03"];
+
+    for(var i = 0; i<3; i++){
+        if(document.getElementById(modes[i]).checked==true){
+            var index=i;
+        }
+    }
+    if(typeof index == 'undefined'){
+        document.getElementById(modes[0]).checked=true;
+    }
+    else{
+        document.getElementById(modes[index]).checked=false;
+        document.getElementById(modes[(index+1)%3]).checked=true;
     }
 }
