51ce84dda2855236a3f6620f4c20373fbd5bfe26
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-10-12 13:35:31 -0300

    [add] logica de los pedales - se detecta el click correctamente

--- amd/src/main.js
+++ amd/src/main.js
@@ -7,8 +7,12 @@ import {
   Termometro,
 } from "./herramientas";
 
-import Maquina  from "./maquina";
+import Maquina from "./maquina";
+// import { drawMam } from "./vista";
 import jQuery from "jquery";
+import { checkBoundingBoxClick } from "./utils";
+
+import { Pedal } from "./pedal";
 
 window.$ = window.jQuery = $ = jQuery;
 
@@ -24,18 +28,29 @@ class Main {
       new Electrometro(),
       new Termometro(),
     ];
-    
-    this.mamografo = new Maquina(0,0,0);
+
+    this.mamografo = new Maquina(0, 0, 0);
     this.c = document.getElementById("canvas");
-    this.c.addEventListener('click',() => this.onCanvasClick() , false);
+    this.c.addEventListener("mousedown", (e) => this.onCanvasClick(e), false);
+    this.c.addEventListener("mouseup", () => this.releaseCanvasClick(), false);
+    // releaseCanvasClick(e) {
+
     this.ctx = this.c.getContext("2d");
     this.cres = document.getElementById("canvRes");
     this.ctxres = this.cres.getContext("2d");
-   
-    
-    
+
     // this.panel_control =
-    
+
+    this.pedalUp = new Pedal(() => {
+      this.mamografo.subirCompresor();
+      this.actualizar();
+    }, [230, 500]);
+
+    this.pedalDown = new Pedal(() => {
+      this.mamografo.bajarCompresor();
+      this.actualizar();
+    }, [170, 500]);
+
     console.log("dibujar iconos");
     this.herr_disponibles.forEach((tool) => {
       let r = $(`<button title= "AD." class="herrams-boton"> </button>`).append(
@@ -46,20 +61,30 @@ class Main {
       r.appendTo("#herramientas-express");
       console.log("dibujar 1");
     });
-    
+
+    this.clickeableOnCanvas = [this.pedalUp, this.pedalDown];
+    this.clicked = null;
+
     this.actualizar();
-   
   }
 
   actualizar() {
-    console.log("Update");
+    // actualizar significa que vamos a dibujar
     this.ctx.clearRect(0, 0, this.c.width, this.c.height);
     this.ctxres.clearRect(0, 0, this.c.width, this.c.height);
+
     //dibujar el mamografo
-    this.mamografo.actualizar(false,this.herr_activas);
+    // this.mamografo.actualizar(false, this.herr_activas);
     // dibujar en el canvas las herramientas nuevas
-    
-    
+
+    // dibujar en el canvas las herramientas nuevas
+    this.herr_activas.forEach((t) => t.dibujar(this.ctx));
+
+    this.pedalDown.dibujar(this.ctx);
+    this.pedalUp.dibujar(this.ctx);
+    this.pedalDown.dibujar(this.ctx);
+    this.mamografo.dibujar(this.ctx);
+
     //dibujar resultados
     this.herr_activas.forEach((t) => t.dibujar_resultado(this.ctxres));
   }
@@ -68,32 +93,47 @@ class Main {
     console.log(tool);
     const i = this.herr_activas.indexOf(tool);
     if (i > -1) {
-      
       this.herr_activas.splice(i, 1);
     } else {
-      
       this.herr_activas.push(tool);
     }
-    
-    this.actualizar();
-    this.mamografo.bajarCompresor();
+
     this.actualizar();
   }
 
   // Este método se levanta cada vez que hay un click en el canvas
   // Checkea que se haya clickeado
-  onCanvasClick() {
-    
+  onCanvasClick(e) {
     console.log("click on canvas");
+    // let rect = this.c.getBoundingClientRect();
     
-   this.mamografo.subirCompresor();
-   this.actualizar();
-  }
+    console.log(e);
+    // let click = [e.pageX - this.c.offsetLeft, e.pageY - this.c.offsetLeft];
+    let click = [e.layerX, e.layerY];
 
-  onCanvasReleaseClick(e) {}
+    for (let index = 0; index < this.clickeableOnCanvas.length; index++) {
+      let elemento = this.clickeableOnCanvas[index];
+      if (elemento.isClicked(click)) {
+        console.log("pedal fue clickeado");
+        this.clicked = elemento;
+        elemento.on_click();
+        break;
+      }
+    }
+  }
+  // Checkea que se elementó se clickeo y activa su callback
+  releaseCanvasClick(e) {
+    console.log("release canvas");
+    if (this.clicked !== null) {
+      this.clicked.on_release();
+      this.clicked = null;
+    }
+
+    this.actualizar();
+  }
 }
 
 export let init = () => {
- let m = new Main();
-console.log("Simulador inicializado");
+  let m = new Main();
+  console.log("Simulador inicializado");
 };
---
+++ amd/src/pedal.js
@@ -0,0 +1,66 @@
+export class Pedal {
+  constructor(hook, posicion = [200, 400], tipo = "up") {
+    this.hook = hook;
+    this.posicion = posicion;
+    this.boundingBox = [30, 60];
+    this.clicked = false;
+    this.loop = null;
+  }
+
+  on_click() {
+    console.log("pedal is clicked jojo");
+    this.clicked = true;
+    this.hook();
+    // this.loop = setInterval(this.hook, 2000);
+  }
+
+  on_release() {
+    console.log("pedal released");
+    this.clicked = false;
+    //clearInterval(this.loop);
+  }
+
+  dibujar(ctx) {
+    ctx.fillStyle = "blue";
+
+    if (this.clicked) {
+      ctx.fillStyle = "red";
+    }
+    ctx.fillRect(
+      this.posicion[0],
+      this.posicion[1],
+      this.boundingBox[0],
+      this.boundingBox[1]
+    );
+  }
+
+  isClicked(click) {
+    console.log(
+      `check if clicked: ${click} - ${this.posicion} - ${this.boundingBox}`
+    );
+    return !(
+      click[0] < this.posicion[0] ||
+      click[0] > this.posicion[0] + this.boundingBox[0] ||
+      click[1] < this.posicion[1] ||
+      click[1] > this.posicion[1] + this.boundingBox[1]
+    );
+  }
+}
+
+//   function checkCanvasClick(elemento, click) {
+//     let x = click[0];
+//     let y = click[1];
+
+//     if (
+//       x < elemento.posicion[0] ||
+//       x > elemento.posicion[0] + elemento.boundingBox[0] ||
+//       y < elemento.posicion[1] ||
+//       y > elemento.posicion[1] + elemento.boundingBox[1]
+//     ) {
+//       return false;
+//     }
+
+//     return true;
+//   }
+
+// }
