caa07ff32408a490fbaf20a09e39e087ccb915d6
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-10-15 12:40:39 -0300

    [ADD] nueva clase ClickeableObject para manejar la lógica de objetos clickeados - se refactoriza pedal.js (puede que tenga que desaparecer)

--- amd/src/pedal.js
+++ amd/src/pedal.js
@@ -1,23 +1,10 @@
-export class Pedal {
-  constructor(hook, posicion = [200, 400], tipo = "up") {
-    this.hook = hook;
-    this.posicion = posicion;
-    this.boundingBox = [30, 60];
-    this.clicked = false;
+import { ClickeableObject } from "./utils";
+export class Pedal extends ClickeableObject{
+  constructor(callback, posicion = [200, 400]) {
+    
+    super(callback, posicion, [30, 60], 200);
     this.loop = null;
   }
-
-  on_click() {
-    this.clicked = true;
-    this.hook();
-    this.loop = setInterval(this.hook, 200);
-  }
-
-  on_release() {
-    this.clicked = false;
-    clearInterval(this.loop);
-  }
-
   dibujar(ctx) {
     ctx.fillStyle = "blue";
 
@@ -29,18 +16,6 @@ export class Pedal {
       this.posicion[1],
       this.boundingBox[0],
       this.boundingBox[1]
-    );
-  }
-
-  isClicked(click) {
-    console.log(
-      `check if clicked: ${click} - ${this.posicion} - ${this.boundingBox}`
-    );
-    return !(
-      click[0] < this.posicion[0] ||
-      click[0] > this.posicion[0] + this.boundingBox[0] ||
-      click[1] < this.posicion[1] ||
-      click[1] > this.posicion[1] + this.boundingBox[1]
     );
   }
 }
