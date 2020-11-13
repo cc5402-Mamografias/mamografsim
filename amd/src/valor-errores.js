a5da138a52e91e9ae792715feeacb3daff9b8e1f
Felipe Canales <felipe.canales.213@gmail.com>
2020-11-13 19:35:21 -0300

    errores pueden ser negativos

--- amd/src/valor-errores.js
+++ amd/src/valor-errores.js
@@ -1,16 +1,18 @@
 var valores = {
     "errorFuerzaEjercida": {
         "valores_base": [0.0, 1.0, 2.5, 3.5, 5.0],
-        "factor_random": 0.5
+        "factor_random": 0.5,
+        "puede_ser_negativo": true
     },
     "errorFuerzaMedida": {
         "valores_base": [0.0, 1.0, 2.0, 3.0, 4.0],
-        "factor_random": 0.2
-
+        "factor_random": 0.2,
+        "puede_ser_negativo": true
     },
     "errorAltura": {
         "valores_base": [0.0, 0.3, 0.7, 1.1, 1.5],
-        "factor_random": 0.2
+        "factor_random": 0.2,
+        "puede_ser_negativo": true
     }
 }
 
@@ -34,5 +36,9 @@ export function getError(tipo, intensidad) {
     let error = valores[tipo]
     let variacion = Math.random() *
         error.factor_random * 2 - error.factor_random; //+- factor
-    return error.valores_base[ind] + variacion;
+    let signo = 1;
+    if (error.puede_ser_negativo && Math.random() < 0.5) {
+        signo = -1;
+    }
+    return signo * (error.valores_base[ind] + variacion);
 }
