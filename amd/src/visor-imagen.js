fe941da69980cf3cf9004156291c86445fa6c34f
José Astorga <jose.n.astorga.tobar@gmail.com>
2020-12-01 19:37:32 -0300

    2 circulos draggable

--- amd/src/visor-imagen.js
+++ amd/src/visor-imagen.js
@@ -21,8 +21,7 @@ export default class VisorImagen {
   }
 
   init() {
-    console.log(this.panel.width());
-    console.log(this.panel.height());
+
     var stage = new Konva.Stage({
       container: 'container-visor',
       width: this.panel.width(),
@@ -30,20 +29,71 @@ export default class VisorImagen {
     });
 
     var layer = new Konva.Layer();
+    stage.add(layer);
 
+    const group = new Konva.Group();
 
-    var rect1 = new Konva.Rect({
-      x: 20,
-      y: 20,
+    layer.add(group);
+
+    var mamImage = new Image();
+
+    mamImage.onload = () => {
+
+      // var originalWidth = mamImage.width;
+
+      var ratio = Math.min((this.panel.width() - 50) / mamImage.width, (this.panel.height() - 200) / mamImage.height);
+      
+      mamImage.width = mamImage.width * ratio;
+      mamImage.height = mamImage.height * ratio;
+
+      var yoda = new Konva.Image({
+        x: stage.getWidth() / 2 - mamImage.width / 2,
+        y: 0,
+        image: mamImage,
+        width: mamImage.width,
+        height: mamImage.height,
+        listening: false
+      });
+
+      
+      group.add(yoda);
+      layer.batchDraw();
+      yoda.moveToBottom();
+
+    };
+
+    mamImage.src = 'img/test/campo_plano_mancha.png'
+
+    var circ1 = new Konva.Circle({
+      x: 100,
+      y: 100,
       width: 100,
       height: 50,
-      fill: 'green',
-      stroke: 'black',
+      fill: 'rgba(0,0,0,0)',
+      stroke: 'red',
       strokeWidth: 4,
+      draggable: true,
     });
-    // add the shape to the layer
-    layer.add(rect1);
+    
+    var circ2 = new Konva.Circle({
+      x: 200,
+      y: 100,
+      width: 100,
+      height: 50,
+      fill: 'rgba(0,0,0,0)',
+      stroke: 'blue',
+      strokeWidth: 4,
+      draggable: true,
+    });
 
-    stage.add(layer);
+    // circ1.zIndex(0);
+    // add the shape to the layer
+    group.add(circ1);
+    group.add(circ2);
+
+    circ2.moveToTop();
+    circ1.moveToTop();
+
+    layer.draw();
   }
 }
