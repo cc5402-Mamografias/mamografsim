c06e8ea9cb371510d14c18b1f211196f3c4fba05
GCarboni96 <gianluca.carboni@ing.uchile.cl>
2020-12-11 17:38:15 -0300

    cambios menores

--- amd/src/plantilla-imagen.js
+++ amd/src/plantilla-imagen.js
@@ -1,3 +1,23 @@
+window.$ = window.jQuery = $ = jQuery;
+
+$("#volver-menu").on('click', () => {
+    $("#contenedor-button").show();
+    $("#contenedor-sim").hide();
+  })
+  $("#loader").remove();
+
+  $("body").on("click", "#volver", function () {
+
+    $("#modal-volver").modal("show");
+
+    //appending modal background inside the contenedor-main div
+    $('.modal-backdrop').appendTo('#contenedor-sim');
+
+    //remove the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
+    $('body').removeClass("modal-open");
+    $('body').css("padding-right", "");
+  });
+
 function show_pr() {
     let x = document.getElementById("plantilla-imagen");
     x.style.display = "block";
