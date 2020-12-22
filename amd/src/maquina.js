d5892829bbd21a7f030bd51a1520fa041f73ba49
GCarboni96 <gianluca.carboni@ing.uchile.cl>
2020-12-22 10:35:21 -0300

    los miliamperios en construirEStado estaban definidos de la forma antigua, se borra la linea de codigo para que simplemente sean definidos por el panel de control

--- amd/src/maquina.js
+++ amd/src/maquina.js
@@ -90,10 +90,7 @@ export default class Maquina {
       kilovolt: (this.kilovolt),
       //para herramienta de fantoma
       miliamperios_nom : (this.miliamperios),
-
       miliamperios: (this.miliamperios),
-
-      miliamperios: this.multiplicar((this.elevar((this.miliamperios),(1+this.errorLinealidad))+ this.mErrorInt(-this.rangemargenmA,this.rangemargenmA)),(1-this.errorRendimiento)),
       miliamperios_auto : this.miliamperios_auto,
 
       filtro: this.filtro,
